f2778140081f34b7f0bda6432b590beada39599c
Dylan Reuter <djreuter45@gmail.com>
2018-12-08 11:32:17 -0600

    added file flags and reading csv from file

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open csv file: %s\n", *csvFileName))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV file.")
+	}
+	fmt.Println(lines)
+}
+
+func parseLines(lines [][]string) []problem {
+
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
